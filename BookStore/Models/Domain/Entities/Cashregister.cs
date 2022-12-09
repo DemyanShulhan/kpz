64e77e6d18cf0377acd8b28a9416f9bcdbcf87c8
DemyanShulhan <demyansh@gmail.com>
2022-12-09 17:09:14 +0300

    task

---
+++ BookStore/Models/Domain/Entities/Cashregister.cs
@@ -0,0 +1,22 @@
+namespace BookStore
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    public partial class Cashregister
+    {
+        [Key]
+        public int IdCashregister { get; set; }
+
+        public int CustomersId_for_Cashregister { get; set; }
+
+        public int BookId_forCashregister { get; set; }
+
+        public virtual Book Book { get; set; }
+
+        public virtual Customer Customer { get; set; }
+    }
+}
